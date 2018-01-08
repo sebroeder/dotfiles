9b12be83753859bde554db34db4e95494928ea1c
Sebastian Röder <sebroeder@users.noreply.github.com>
2018-01-08 10:05:38 +0100

    Set XDG_CONFIG_HOME explicitly to fix shell integration in Jetbrains IDEs

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -2,6 +2,10 @@
 set --export LANG en_US.UTF-8
 set --export LC_CTYPE en_US.UTF-8
 
+# Set XDG_CONFIG_HOME explicitly to fix shell integration in Jetbrains IDEs
+# https://youtrack.jetbrains.com/issue/IDEA-169111  
+set --export XDG_CONFIG_HOME $HOME/.config
+
 # Load rbenv automatically
 status --is-interactive; and source (rbenv init -|psub)
 
